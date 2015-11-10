6df7951c2a38857eb341f78c930bc9ece24db88f
Carson Daniel Parker <Carson@dkparker.com>
2015-11-10 13:08:47 -0800

    first commit.

---
+++ app.js
@@ -0,0 +1,75 @@
+// array of photo objects : photo collect []
+
+
+function Photo(site, path) {
+// sites path and votes property
+// pushing this object thats being created into the function
+
+}
+
+// returns a random index : calc random
+
+// make sure two indices are different : do while
+calc
+
+// vote funtion
+vote = function() {
+  console.log(photoCollect[].site + ' had ' + photoCollect[].votes + ' votes. ');
+  photoCollect[].votes++; // here is where the vote increments
+  console.log(photoCollect[.site] + ' now has ' + photoCollect[].votes + ' votes. ');
+
+  picture1.parentNode.removeChild(picture1);
+
+  displayPhotos();
+}
+
+var picture1 = document.createElementById('img1');
+
+
+//display photos function
+//make a second picture
+var index1 =
+var index2 =
+var somethingThatLinksToHtml
+
+
+picture1 = document.createElement('img');
+
+picture1.setAttribute('id', 'img1');
+
+picture1.setAttribute('src', photocollect[index1].path);
+
+picture1.setAttribute('width', '300');
+
+somethingThatLinksToHtml.appendChild(picture1);
+
+//add event listeners
+picture1.addEventListener('click', function(){
+  vote(index1); // An integer being past through
+});
+
+// var something = new Photo('something', 'imgages/picture.png')
+
+//displayPhotos();
+
+var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+]
+
