6202a3c4abf2469418454db826e49e352ec7b8ad
Carson Daniel Parker <Carson@dkparker.com>
2015-11-10 16:30:52 -0800

    pictures now work on the browser and event listeners work.

--- app.js
+++ app.js
@@ -1,94 +1,62 @@
-// array of photo objects
-var gamePhoto = [];
+var gamePhotos = [];
 
-// pushing this object thats being created into the function
-// sites path and votes property
-function GamePhoto(site, path) {
-  this.site = site;
+function GamePhoto(name, path) {
+  this.name = name;
   this.path = path;
   this.votes = 0;
-  gamePhoto.push(this);
+  gamePhotos.push(this);
 }
+
+var display1 = document.getElementById('displayImg1');
+var display2 = document.getElementById('displayImg2');
 
 var tracker = {
-  photo1: 0,
-  photo2: 0,
+  randomNumberOne: null,
+  randomNumberTwo: null,
   calcRandNum: function() {
-  return Math.floor(Math.random() * gamePhoto.length);
+    return Math.floor(Math.random() * gamePhotos.length);
   },
-  getRandomImg: function (){
-    this.index1 = gamePhoto[this.calcRandNum()];
-    this.index2 = gamePhoto[this.calcRandNum()];
-    var img1 = this.index1.path;
-    var img2 = this.index2.path;
-    var display1 = document.getElementById ('displayImg1');
-    var display2 = document.getElementById ('displayImg2');
-    while (img1 === img2) {
-      this.index2 = calcRandNum();
-      img2 = gamePhoto[this.index2].path;
+  displayNewImages: function() {
+    this.randomNumberOne = this.calcRandNum();
+    this.randomNumberTwo = this.calcRandNum();
+
+    while (this.randomNumberOne === this.randomNumberTwo) {
+      this.randomNumberTwo = this.calcRandNum();
     };
-    display1.src = img1;
-    display2.src = img2;
-    },
-  }
 
-function callBack (event) {
-  this.photo1.vote++;
+    var img1 = gamePhotos[this.randomNumberOne];
+    var img2 = gamePhotos[this.randomNumberTwo];
+
+    display1.src = img1.path;
+    display2.src = img2.path;
+
+    display1.addEventListener('click', this.voteForOne);
+    display2.addEventListener('click', this.voteForTwo);
+  },
+  voteForOne: function() {
+    gamePhotos[tracker.randomNumberOne].votes +=1;
+    tracker.displayNewImages();
+  },
+  voteForTwo: function() {
+    gamePhotos[tracker.randomNumberTwo].votes +=1;
+    tracker.displayNewImages();
+  },
 }
-// vote funtion
-vote = function() {
-  console.log(gamePhoto[photo1].site + ' had ' + gamePhoto[photo1].votes + ' votes. ');
-  gamePhoto[photo1].votes++; // here is where the vote increments
-  console.log(gamePhoto[photo2].site + ' now has ' + gamePhoto[photo2].votes + ' votes. ');
-  gamePhoto[photo2].votes++;
 
-  picture1.parentNode.removeChild(picture1);
+var destiny = new GamePhoto('destiny', 'images/destiny.png')
+var diablo = new GamePhoto('diablo', 'images/diablo.png')
+var guildwars = new GamePhoto('guildwars', 'images/guildwars.png')
+var hearthstone = new GamePhoto('hearthstone', 'images/hearthstone.png')
+var heroes = new GamePhoto('heroes', 'images/heroes.png')
+var lol = new GamePhoto('lol', 'images/lol.png')
+var overwatch = new GamePhoto('overwatch', 'images/overwatch.png')
+var pokemon = new GamePhoto('pokemon', 'images/pokemon.png')
+var portal = new GamePhoto('portal', 'images/portal.png')
+var starcraft = new GamePhoto('starcraft', 'images/starcraft.png')
+var supersmash = new GamePhoto('supersmash', 'images/supersmash.png')
+var wow = new GamePhoto('wow', 'images/wow.png')
 
-  displayPhotos();
-}
-
-var picture1 = document.createElementById('img1');
-var picture2 = document.createElementById('img2');
-
-//display photos function
-//make a second picture
-displayPhotos = function() {
-  var somethingThatLinksToHtml
-
-
-  picture1 = document.createElement('img');
-  picture2 = document.createElement('img');
-
-  picture1.setAttribute('id', 'img1');
-  picture2.setAttribute('id', 'img2');
-
-  picture1.setAttribute('src', gamePhoto[photo1].path);
-  picture2/setAttribute('src', gamePhoto[photo2].path);
-
-  picture1.setAttribute('width', '300');
-  picture2.setAttribute('width', '300');
-
-  somethingThatLinksToHtml.appendChild(picture1);
-
-//   //add event listeners
-  picture1.addEventListener('click', function(){
-    vote(index1); // An integer being past through
-  });
-}
-var destiny = new Photo('destiny', 'images/destiny.png')
-var diablo = new Photo('diablo', 'images/diablo.png')
-var guildwars = new Photo('guildwars', 'images/guildwars.png')
-var hearthstone = new Photo('hearthstone', 'images/hearthstone.png')
-var heroes = new Photo('heroes', 'images/heroes.png')
-var lol = new Photo('lol', 'images/lol.png')
-var overwatch = new Photo('overwatch', 'images/overwatch.png')
-var pokemon = new Photo('pokemon', 'images/pokemon.png')
-var portal = new Photo('portal', 'images/portal.png')
-var starcraft = new Photo('starcraft', 'images/starcraft.png')
-var supersmash = new Photo('supersmash', 'images/supersmash.png')
-var wow = new Photo('wow', 'images/wow.png')
-
-//displayPhotos();
+tracker.displayNewImages();
 
 var data = [
     {
